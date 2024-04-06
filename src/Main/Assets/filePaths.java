c7aefccc9141213631624c37fa72df176e8097fd
Ethan <60994567+Tank-203@users.noreply.github.com>
2024-04-05 19:13:30 -0500

    Assets

---
+++ src/Main/Assets/filePaths.java
@@ -0,0 +1,12 @@
+package src.Main.Assets;
+
+public class filePaths {
+    public static String UiImgPath = "./src/Main/Assets/UIAssets/";
+    public static String photoPath = "./src/Main/Assets/VicPhotos/";
+
+    public static String vicList = "ListOfVictims.txt";
+    public static String uiTheme = "UITheme.txt";
+    public static String questList = "Questions.txt";
+
+    public static String saveFilePath = "./src/Main/SaveFiles/";
+}
