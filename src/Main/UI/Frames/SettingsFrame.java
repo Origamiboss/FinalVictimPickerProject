7525166d93172dba9ce85367fa42317eea6403ff
SilverCircuits <121541637+c-a-parr@users.noreply.github.com>
2024-04-17 18:24:03 -0500

    Changed SettingsFrame to match UI.

--- src/Main/UI/Frames/SettingsFrame.java
+++ src/Main/UI/Frames/SettingsFrame.java
@@ -1,13 +1,18 @@
 package Main.UI.Frames;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import Main.Holder;
+import Main.UI.Format.VicFormatter;
+import UIElements.Buttons.RoundButton;
 import UIElements.Colors.CurrentUITheme;
 import UIElements.Colors.UIColors;
+import UIElements.Panels.RoundedPanel;
+import UIElements.TextCanvas;
 
 public class SettingsFrame extends JFrame {
     final private JFrame self = this;
@@ -17,9 +22,9 @@ public class SettingsFrame extends JFrame {
 
         //make the holder panel
         self.setTitle("Settings");
-        JPanel statHolder = new JPanel();
+        RoundedPanel statHolder = new RoundedPanel(h.getTheme()); // UI adjustment
         self.add(statHolder);
-        statHolder.setLayout(new GridLayout(4,1));
+        statHolder.setLayout(new BoxLayout(statHolder,BoxLayout.Y_AXIS)); // UI adjustment
 
         //create each stat and put it inside its own panel
         //Color changer
@@ -28,7 +33,7 @@ public class SettingsFrame extends JFrame {
             optionsToChoose[i] = UIColors.colors[i];
         }
 
-        JPanel backcolorHolder = new JPanel();
+        RoundedPanel backcolorHolder = new RoundedPanel(h.getTheme()); // UI adjustment
         backcolorHolder.add(new JLabel("Background:"));
         // Creating a JComboBox and setting its bounds
         JComboBox<String> backComboBox = new JComboBox<>(optionsToChoose);
@@ -39,20 +44,30 @@ public class SettingsFrame extends JFrame {
         statHolder.add(backcolorHolder);
 
         //foreground
-        JPanel frontcolorHolder = new JPanel();
-        frontcolorHolder.add(new JLabel("Foreground:"));
+        RoundedPanel frontColorHolder = new RoundedPanel(h.getTheme()); // UI adjustment
+        frontColorHolder.add(new JLabel("Foreground:"));
         // Creating a JComboBox and setting its bounds
         JComboBox<String> frontComboBox = new JComboBox<>(optionsToChoose);
         frontComboBox.setBounds(80, 50, 140, 20);
         //set the start value
         frontComboBox.setSelectedItem(holder.getTheme().getForegroundString());
-        frontcolorHolder.add(frontComboBox);
-        statHolder.add(frontcolorHolder);
+        frontColorHolder.add(frontComboBox);
+        statHolder.add(frontColorHolder);
 
         //get the JComboBox String with jComboBox.getItemAt(jComboBox.getSelectedIndex());
-        statHolder.add(new JLabel("The changes will occur the next time you open the program."));
-        JButton doneButton = new JButton("Save");
-        statHolder.add(new JPanel().add(doneButton));
+        // UI adjustments
+        TextCanvas displayCanvas = new TextCanvas(h.getTheme(), 15, false);
+        displayCanvas.setText("To save your changes restart the program!");
+        displayCanvas.setSize(new Dimension(500, 15));
+        displayCanvas.setBorder(new LineBorder(h.getTheme().getCurrentForegroundColor().main(), 2));
+        VicFormatter canvasFormat = new VicFormatter(displayCanvas, 5);
+        statHolder.add(canvasFormat.getPanel()); // UI adjustments
+        RoundButton doneButton = new RoundButton("Save", h.getTheme());
+        doneButton.setSize(new Dimension(300, 50));
+        VicFormatter buttonFormat = new VicFormatter(doneButton, 5);
+        statHolder.add(buttonFormat.getPanel());
+        // UI adjustments end.
+
         //Save stats
         doneButton.addActionListener(new ActionListener() {
             @Override
